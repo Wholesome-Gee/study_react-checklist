95f0d4262fe17f7132ee7c83b72e8cdc31e29561
Wholesome-Gee <jiyong0419@naver.com>
2025-04-25 13:22:23 +0900

    프로젝트 초기화

---
+++ src/theme.ts
@@ -0,0 +1,16 @@
+import { DefaultTheme } from "styled-components/dist/types";
+
+const lightTheme: DefaultTheme = {
+  backgroundColor: "#f0f0f0",
+  textColor: "#3c3c3c",
+  accentColor: "#0074BA",
+  boxColor: "#ddd",
+};
+const darkTheme: DefaultTheme = {
+  backgroundColor: "#3c3c3c",
+  textColor: "#f0f0f0",
+  accentColor: "#FCD53F",
+  boxColor: "#161F28",
+};
+
+export { lightTheme, darkTheme };
